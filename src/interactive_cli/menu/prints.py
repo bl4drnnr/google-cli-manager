064dab25936e3879d74ad1b83d0b25c618f3317a
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-10-13 16:51:12 +0200

    peaces of documentation

--- src/interactive_cli/menu/prints.py
+++ src/interactive_cli/menu/prints.py
@@ -23,14 +23,45 @@ def print_introduction(stdscr):
     stdscr.addstr('If you have any issues with access (you\'ve got an error)\n')
     stdscr.addstr('contact your Google Admin Workspace administrator to obtain proper accesses.\n\n')
 
+    stdscr.addstr('Use ')
+    stdscr.addstr('ARROWS', curses.A_BOLD)
+    stdscr.addstr(' on your keyboard for navigations.\n')
+    stdscr.addstr('Press ')
+    stdscr.addstr('ENTER', curses.A_BOLD)
+    stdscr.addstr(' to confirm your choice.\n\n')
+
 
 def print_documentation(stdscr):
-    pass
+    print_logo(stdscr, 2)
+    stdscr.addstr('All functions and deeper descriptions were already described\n')
+    stdscr.addstr('in ')
+    stdscr.addstr('README', curses.A_BOLD)
+    stdscr.addstr(' file. Below will be listed only short definition for them\n')
+    stdscr.addstr('but, most important, how to work with different types of\n')
+    stdscr.addstr('Google Accounts', curses.A_BOLD)
+    stdscr.addstr(' like ')
+    stdscr.addstr('Service Account', curses.A_BOLD)
+    stdscr.addstr(' and what type of credentials\n')
+    stdscr.addstr('you will need in order to obtain access to different endpoints.\n\n')
+
+    stdscr.addstr('DESCRIPTION OF AVAILABLE FUNCTIONS\n\n')
+    stdscr.addstr('CREDENTIALS\n\n')
+
+    stdscr.addstr('In case of any issues, don\'t hesitate to text me - ')
+    stdscr.addstr('mikhail.bahdashych@protonmail.com\n\n', curses.A_BOLD | curses.A_UNDERLINE)
+
+    stdscr.addstr('Press any key to continue...')
+    stdscr.getch()
 
 
 def print_exit(stdscr):
-    print_logo(stdscr, 3)
-    stdscr.addstr('Bye...\n\n')
+    print_logo(stdscr, 4)
+    stdscr.addstr('Hope you had fun. Bye...\n\n', curses.A_BOLD)
+
+    stdscr.addstr('In order to obtain more information about Google API\n')
+    stdscr.addstr('or other SDKs see - ')
+    stdscr.addstr('https://developers.google.com\n\n', curses.A_UNDERLINE)
+
     stdscr.addstr('Press any key to exit...')
     stdscr.getch()
     sys.exit()
@@ -45,11 +76,15 @@ def print_menu(stdscr, current_row_idx):
         if idx == current_row_idx:
             if row == 'Exit\n':
                 stdscr.addstr(f' > {row}', curses.color_pair(3))
+            elif row == 'Start\n':
+                stdscr.addstr(f' > {row}', curses.color_pair(2))
             else:
-                stdscr.addstr(f' > {row}', curses.color_pair(1))
+                stdscr.addstr(f' > {row}')
         else:
             if row == 'Exit\n':
                 stdscr.addstr(row, curses.color_pair(3))
+            elif row == 'Start\n':
+                stdscr.addstr(row, curses.color_pair(2))
             else:
                 stdscr.addstr(row)
 
