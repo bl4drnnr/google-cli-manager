71594506f93aea46a379dd7105f23107fc5d9b91
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-10-19 16:05:43 +0200

    installation script and requirements txt fix

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 repository="https://github.com/bl4drnnr/google-cli-manager.git"
 
-echo "Google-Manager CLI installation..."
+echo "Google Manager CLI installation..."
 
 cd "$HOME"
 mkdir -p "$HOME/.gmcli"
@@ -10,13 +10,13 @@ cd "$HOME/.gmcli"
 
 git clone "$repository"
 
-cd "$HOME/github-cli-manager"
+cd "$HOME/.gmcli/google-cli-manager"
 
 for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zprofile" ".zshrc"
     do
       echo "alias gmcli='python3 ${HOME}/.gmcli/google-cli-manager/main.py'" >> "${HOME}/${EACH_PROFILE}"
     done
 
-pip install -r "$HOME/.gmcli/requirements.txt"
+/usr/bin/pip3 install -r "$HOME/.gmcli/google-cli-manager/requirements.txt"
 
-echo "Google-Manager CLI have been successfully installed..."
+echo "Google Manager CLI have been successfully installed..."
