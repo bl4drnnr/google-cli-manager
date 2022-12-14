2140fd332b4ac90a8c818c42233a2af0979fa9e9
bl4drnnr <mikhail.bahdashych@protonmail.com>
2022-12-14 22:27:21 +0100

    delete script

--- src/cli/uninstall.sh
+++ src/cli/uninstall.sh
@@ -1,12 +1,10 @@
 #!/usr/bin/env bash
 
 rm -r "$HOME/.gmcli"
-path_to_purge="alias gmcli='python3 ${HOME}/.gmcli/google-cli-manager/main.py'"
 
 for EACH_PROFILE in ".profile" ".bashrc" ".bash_profile" ".zprofile" ".zshrc"
   do
-    sed -i "/${path_to_purge}/d" ~/${EACH_PROFILE}
+    sed -i '' -e '\|^alias gmcli|d' ~/${EACH_PROFILE}
   done
 
 echo "Google Manager CLI has been successfully deleted successfully..."
-
